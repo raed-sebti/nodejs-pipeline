c82d35848e485ae1c01965a69917543dc84f53ec
Raed SBEIT <sebti.raed@gmail.com>
2020-07-04 15:25:18 -0400

    changed

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-// load the http module
-var http = require('http');
-//
-// // configure our HTTP server
-var server = http.createServer(function (request, response) {
-   response.writeHead(200, {"Content-Type": "text/plain"});
-   response.end("Hello getintodevops.com\n");
-});
+'use strict';
 
-// listen on localhost:8000
-   server.listen(8000);
-   console.log("Server listening at http://127.0.0.1:8000/");
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+  });
+
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
